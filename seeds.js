b08a5b0a987f29a8ad68b1289d211663dd2506d7
Liam Pimlott <liam.tj.pimlott@gmail.com>
2017-01-28 20:30:00 +0000

    First commit

---
+++ seeds.js
@@ -0,0 +1,59 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var data = [
+    {
+        name : "Clouds Rest", 
+        image: "https://farm6.staticflickr.com/5181/5641024448_04fefbb64d.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        name : "Rooster Hollow", 
+        image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+    {
+        name : "Pilgrims Cove", 
+        image: "https://farm4.staticflickr.com/3270/2617191414_c5d8a25a94.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur"
+    },
+]
+function seedDB(){
+    //Remove all campgrounds
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        } else {
+            //add a few campgrounds
+            data.forEach(function(seed){
+                Campground.create(seed, function(err, campground){
+                    if(err){
+                       console.log(err);
+                    } else {
+                       console.log("Added Campground.");
+                       //Create a Comment
+                       Comment.create(
+                           {
+                               text: "TEST TEST THIS IS A TEST COMMENT",
+                               author: "yo mama"
+                           }, function(err, comment){
+                               if(err){
+                                   console.log("Err")
+                               } else {
+                                   campground.comments.push(comment);
+                                   campground.save();
+                                   console.log("New comment added")
+                               }
+                           }
+                           )
+                    }
+                });
+            });
+            console.log("removed old campgrounds");
+        }
+    });  
+    //add a few comments
+}
+
+module.exports = seedDB;
